ea60c5336ca877f81193d7b9dbdc3d9e1cef47e0
milan.zivanov <milan.zivanov@gmail.com>
2019-03-22 11:31:19 +0100

    deliting post

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -46,6 +46,23 @@ export class PostsComponent implements OnInit {
       });
   }
 
+  // updatePost(post) {
+  //   this.http.put(this.url, JSON.stringify(post))
+  //   // this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+  //     .subscribe( response => {
+  //       console.log(response);
+  //     })
+  // }
+
+  delitePost(post: ITemplate) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        console.log('index:', index)
+        this.posts.splice(index, 1);
+      })
+  }
+
 }
 
 
